c0829d5b2bf6191e4f65b2c72ba761ef0a9fa4e8
Rizky Hanifudin <41834573+rizky-hanifudin@users.noreply.github.com>
2026-08-10 09:49:40 +0700

    Update config.js

--- album/config.js
+++ album/config.js
@@ -4,7 +4,7 @@
 
 const CONFIG = {
   // Google Apps Script URL (GANTI DENGAN URL DEPLOYMENT ANDA)
-  API_URL: 'https://script.google.com/macros/s/AKfycbwLhC_d-_FXh9KN1c4dBRJ_cKg9yHGywNX8OLNpHRTWZ22rpowYY-iqFOyAWbPjFt_O/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbyTgWnTxFRqk3ALz8EKqjGbAENlAMXafqjpHNKb4p1uFJ2xPyDG7idBafSxykSdq3Qx/exec',
   
   // Default wedding ID (bisa di-override via URL parameter)
   DEFAULT_WEDDING_ID: 'demo',
